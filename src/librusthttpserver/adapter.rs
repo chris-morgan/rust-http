72953863a117b77b5d5485adae16fd80b3b0f2e8
Chris Morgan <me@chrismorgan.info>
2013-07-16 16:01:10 +1000

    Remove dead code.

--- src/librusthttpserver/adapter.rs
+++ src/librusthttpserver/adapter.rs
@@ -7,25 +7,6 @@ use extra::{net, uv_iotask};
 
 static READ_SIZE : uint = 1024;
 
-/// A wrapper of an `std::io::Writer` into an `std::rt::io::Writer`.
-pub struct WriterRtWriterAdapter<T> {
-    priv writer: T,
-}
-
-pub fn WriterRtWriterAdapter<T: io::Writer>(writer: T) -> WriterRtWriterAdapter<T> {
-    WriterRtWriterAdapter { writer: writer }
-}
-
-impl<T: io::Writer> rt::io::Writer for WriterRtWriterAdapter<T> {
-    fn write(&mut self, buf: &[u8]) {
-        self.writer.write(buf);
-    }
-
-    fn flush(&mut self) {
-        self.writer.flush();
-    }
-}
-
 /// Adapter of extra::net stuff to std::rt::io::net stuff
 pub struct ExtraNetTcpListener {
     accept_ch: Chan<()>,
