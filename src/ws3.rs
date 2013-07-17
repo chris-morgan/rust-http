0cd10d1c891152115045be3637c6ccb45e1022a1
Chris Morgan <me@chrismorgan.info>
2013-07-17 17:58:58 +1000

    Tidy sample server up and add some more forms of server.

--- src/ws3.rs
+++ src/ws3.rs
@@ -1,88 +1,80 @@
 extern mod extra;
 extern mod rusthttpserver;
 
-use rusthttpserver::server::{Server, Config, ServerUtil};
+use rusthttpserver::server::serve_forever;
 use rusthttpserver::request::Request;
 use rusthttpserver::response::ResponseWriter;
 use std::rt::io::net::ip::Ipv4;
 use std::rt::io::Writer;
 
-struct MyServer;
+/// A copy of a request from Apache's default thingummy
+fn apache_fake_handler(_r: Request, mut w: ResponseWriter) {
+    w.headers.insert(~"Date", ~"Tue, 16 Jul 2013 03:43:34 GMT");
+    w.headers.insert(~"Server", ~"Apache/2.2.22 (Ubuntu)");
+    w.headers.insert(~"Last-Modified", ~"Thu, 05 May 2011 11:46:42 GMT");
+    w.headers.insert(~"ETag", ~"\"501b29-b1-4a285ed47404a\"");
+    w.headers.insert(~"Accept-Ranges", ~"bytes");
+    w.headers.insert(~"Content-Length", ~"177");
+    w.headers.insert(~"Vary", ~"Accept-Encoding");
+    w.headers.insert(~"Content-Type", ~"text/html");
+    w.headers.insert(~"X-Pad", ~"avoid browser bug");
 
-impl Server for MyServer {
-	pub fn get_config(&self) -> Config {
-		Config {
-			bind_address: Ipv4(127, 0, 0, 1, 8001),
-		}
-	}
-
-    fn handle_request(&self, request: Request, mut r: ResponseWriter) {
-        //r.headers.insert(~"Content-Length", ~"15");
-        //r.headers.insert(~"Content-Type", ~"text/plain; charset=UTF-8");
-        //r.headers.insert(~"Server", ~"Example");
-        //r.headers.insert(~"Date", ~"Wed, 17 Apr 2013 12:00:00 GMT");
-        //r.write(bytes!("Hello, World!"));
-
-
-        r.headers.insert(~"Date", ~"Tue, 16 Jul 2013 03:43:34 GMT");
-        r.headers.insert(~"Server", ~"Apache/2.2.22 (Ubuntu)");
-        r.headers.insert(~"Last-Modified", ~"Thu, 05 May 2011 11:46:42 GMT");
-        r.headers.insert(~"ETag", ~"\"501b29-b1-4a285ed47404a\"");
-        r.headers.insert(~"Accept-Ranges", ~"bytes");
-        r.headers.insert(~"Content-Length", ~"177");
-        r.headers.insert(~"Vary", ~"Accept-Encoding");
-        r.headers.insert(~"Content-Type", ~"text/html");
-        r.headers.insert(~"X-Pad", ~"avoid browser bug");
-
-        r.write(bytes!("<html><body><h1>It works!</h1>
+    w.write(bytes!("<html><body><h1>It works!</h1>
 <p>This is the default web page for this server.</p>
 <p>The web server software is running but no content has been added, yet.</p>
 </body></html>\n"));
-    }
+}
 
-	/*fn handle_request(&self, request: Request, mut r: ResponseWriter) {
-        r.headers.insert(~"Content-Type", ~"text/html");
-        r.headers.insert(~"Server", ~"Rust Thingummy/0.0-pre");
-        r.write(bytes!("<!DOCTYPE html><title>Rust HTTP server</title>"));
+fn main() {
+    serve_forever(Ipv4(127, 0, 0, 1, 8001), apache_fake_handler);
 
-		r.write(bytes!("<h1>Request</h1>"));
+    do serve_forever(Ipv4(127, 0, 0, 1, 8001)) |r, mut w| {
+        w.headers.insert(~"Content-Type", ~"text/html");
+        w.headers.insert(~"Server", ~"Rust Thingummy/0.0-pre");
+        w.write(bytes!("<!DOCTYPE html><title>Rust HTTP server</title>"));
+
+        w.write(bytes!("<h1>Request</h1>"));
         let s = fmt!("<dl>
             <dt>Method</dt><dd>%s</dd>
             <dt>Path</dt><dd>%s</dd>
             <dt>HTTP version</dt><dd>%?</dd>
             <dt>Close connection</dt><dd>%?</dd></dl>",
-            request.method.to_str(),
-            request.path,
-            request.version,
-            request.close_connection);
-        r.write(s.as_bytes().to_owned());
-        r.write(bytes!("<h2>Headers</h2>"));
-        r.write(bytes!("<table><thead><tr><th>Name</th><th>Value</th></thead><tbody>"));
-        for request.headers.iter().advance |(k, v)| {
+            r.method.to_str(),
+            r.path,
+            r.version,
+            r.close_connection);
+        w.write(s.as_bytes().to_owned());
+        w.write(bytes!("<h2>Headers</h2>"));
+        w.write(bytes!("<table><thead><tr><th>Name</th><th>Value</th></thead><tbody>"));
+        for r.headers.iter().advance |(k, v)| {
             let line = fmt!("<tr><td><code>%s</code></td><td><code>%s</code></td></tr>", *k, *v);
-            r.write(line.as_bytes().to_owned());
+            w.write(line.as_bytes().to_owned());
         }
-		r.write(bytes!("</tbody></table>"));
-        r.write(bytes!("<h2>Body</h2><pre>"));
-        r.write(request.body.as_bytes().to_owned());
-        r.write(bytes!("</pre>"));
+        w.write(bytes!("</tbody></table>"));
+        w.write(bytes!("<h2>Body</h2><pre>"));
+        w.write(r.body.as_bytes().to_owned());
+        w.write(bytes!("</pre>"));
 
-		r.write(bytes!("<h1>Response</h1>"));
-        let s = fmt!("<dl><dt>Status</dt><dd>%s</dd></dl>", r.status.to_str());
-		r.write(s.as_bytes().to_owned());
-		r.write(bytes!("<h2>Headers</h2>"));
-        r.write(bytes!("<table><thead><tr><th>Name</th><th>Value</th></thead><tbody>"));
+        w.write(bytes!("<h1>Response</h1>"));
+        let s = fmt!("<dl><dt>Status</dt><dd>%s</dd></dl>", w.status.to_str());
+        w.write(s.as_bytes().to_owned());
+        w.write(bytes!("<h2>Headers</h2>"));
+        w.write(bytes!("<table><thead><tr><th>Name</th><th>Value</th></thead><tbody>"));
         {
-            let h = copy r.headers;
+            let h = copy w.headers;
             for h.iter().advance |(k, v)| {
                 let line = fmt!("<tr><td><code>%s</code></td><td><code>%s</code></td></tr>", *k, *v);
-                r.write(line.as_bytes().to_owned());
+                w.write(line.as_bytes().to_owned());
             }
         }
-		r.write(bytes!("</tbody></table>"));
-	}*/
-}
+        w.write(bytes!("</tbody></table>"));
+    }
 
-fn main() {
-	println(fmt!("Serve finished: %?", MyServer.serve_forever()));
+    do serve_forever(Ipv4(127, 0, 0, 1, 8001)) |_r, mut w| {
+        w.headers.insert(~"Content-Length", ~"15");
+        w.headers.insert(~"Content-Type", ~"text/plain; charset=UTF-8");
+        w.headers.insert(~"Server", ~"Example");
+        w.headers.insert(~"Date", ~"Wed, 17 Apr 2013 12:00:00 GMT");
+        w.write(bytes!("Hello, World!"));
+    }
 }
