5522bd2b9495040103c38c659f33dac172866e94
Chris Morgan <me@chrismorgan.info>
2013-07-17 17:58:13 +1000

    "Improve server." ;-)

--- src/librusthttpserver/server.rs
+++ src/librusthttpserver/server.rs
@@ -2,10 +2,9 @@
 
 extern mod extra;
 
-use std::option::Option;
-use std::rt::io::{Listener};
-use std::rt::io::net::ip::IpAddr;
-use std::rt::io::{io_error, IoError};
+use std::rt::io::Listener;
+use std::rt::io::net::ip::{IpAddr, Ipv4};
+use std::rt::io::io_error;
 
 #[cfg(newrt)]
 pub use std::rt::io::net::tcp::{TcpListener, TcpStream};
@@ -30,6 +29,8 @@ pub trait Server {
 	pub fn get_config(&self) -> Config;
 }
 
+/// A temporary trait to fix current deficiencies in Rust's default methods on traits.
+/// You'll need to import `ServerUtil` to be able to call `serve_forever` on a Server.
 pub trait ServerUtil {
     pub fn serve_forever(self);
 }
@@ -55,43 +56,38 @@ impl<T: Send + Server> ServerUtil for T {
                 loop {
                     // OK, we're sort of shadowing an IoError here. Perhaps this should be done in a
                     // separate task so that it can safely fail...
-                    let mut error : Option<IoError> = None;
-                    let mut optstream : Option<TcpStream> = None;
-                    io_error::cond.trap(|e: IoError| {
+                    let mut error = None;
+                    let optstream = io_error::cond.trap(|e| {
                         error = Some(e);
                     }).in(|| {
-                        optstream = listener.accept();
+                        listener.accept()
                     });
-                    match optstream {
-                        None => {
-                            debug!("accept failed: %?", error);
-                            // Question: is this the correct thing to do? We should probably be more
-                            // intelligent, for there are some accept failures that are likely to be
-                            // permanent, such that continuing would be a very bad idea, such as
-                            // ENOBUFS/ENOMEM; and some where it should just be ignored, e.g.
-                            // ECONNABORTED. TODO.
-                            loop;
-                        },
-                        Some(stream) => {
-                            debug!("accepted connection, got %?", stream);
-                            let stream = ::std::cell::Cell::new(stream);
-                            do spawn {
-                                let mut stream = ~stream.take();
-                                //RequestBuffer::new(stream);
-                                let request = Request::get(~RequestBuffer::new(stream));
-                                let mut response = ResponseWriter::new(*stream);
-                                match request {
-                                    Ok(request) => {
-                                        self.handle_request(request, response);
-                                        // Sorry, only single-threaded at present:
-                                        // blocked on mozilla/rust#7661
-                                    },
-                                    Err(status) => {
-                                        response.status = status;
-                                        response.write_headers();
-                                    },
-                                }
-                            }
+                    if optstream.is_none() {
+                        debug!("accept failed: %?", error);
+                        // Question: is this the correct thing to do? We should probably be more
+                        // intelligent, for there are some accept failures that are likely to be
+                        // permanent, such that continuing would be a very bad idea, such as
+                        // ENOBUFS/ENOMEM; and some where it should just be ignored, e.g.
+                        // ECONNABORTED. TODO.
+                        loop;
+                    }
+                    let stream = ::std::cell::Cell::new(optstream.unwrap());
+                    do spawn {
+                        let mut stream = ~stream.take();
+                        debug!("accepted connection, got %?", stream);
+                        //RequestBuffer::new(stream);
+                        let request = Request::get(~RequestBuffer::new(stream));
+                        let mut response = ResponseWriter::new(*stream);
+                        match request {
+                            Ok(request) => {
+                                self.handle_request(request, response);
+                                // Sorry, only single-threaded at present:
+                                // blocked on mozilla/rust#7661
+                            },
+                            Err(status) => {
+                                response.status = status;
+                                response.write_headers();
+                            },
                         }
                     }
                 }
@@ -100,44 +96,68 @@ impl<T: Send + Server> ServerUtil for T {
     }
 }
 
+/// The necessary configuration for an HTTP server.
+///
+/// At present, only the IP address and port to bind to are needed, but it's possible that other
+/// options may turn up later.
 pub struct Config {
 	bind_address: IpAddr,
 }
 
-/*
+/// A simple `Server`-implementing class, allowing code to be written in an imperative style.
 pub struct SimpleServer {
-	config: Config,
-}
-
-impl Server for SimpleServer {
-	pub fn get_config(&self) -> Config {
-		self.config
-	}
+    config: Config,
+    handler: ~fn(Request, ResponseWriter),
 }
 
 impl SimpleServer {
-	/// Create a `Server` bound to all IPv4 addresses on port 80
-	#[inline]
-	pub fn new_public(callback: &'self T) -> Server<'self, T> {
-		Server {
-			bind_address: ip::v4::parse_addr("0.0.0.0"),
-			port: 80,
-			backlog: 100,
-			callback: callback,
-		}
-	}
-
-	/// Create a `Server` with the specified config parameters
-	#[inline]
-	pub fn new(bind_address: ip::IpAddr, port: uint, callback: &'self T) -> Server<'self, T> {
-		SimpleServer {
-			config: Config {
-				bind_address: bind_address,
-				port: port,
-				backlog: 100,
-				callback: callback,
-			}
-		}
-	}
+    /// Create a new `SimpleServer` instance with the provided members.
+    pub fn new(config: Config, handler: ~fn(Request, ResponseWriter)) -> SimpleServer {
+        SimpleServer {
+            config: config,
+            handler: handler,
+        }
+    }
 }
-*/
+
+impl Server for SimpleServer {
+    /// Handles a request by passing it on to the structure's handler function.
+    #[inline]
+	pub fn handle_request(&self, request: Request, response: ResponseWriter) {
+        (self.handler)(request, response);
+    }
+
+    /// Returns the structure's known config.
+    #[inline]
+	pub fn get_config(&self) -> Config {
+        self.config
+    }
+}
+
+/// Create a simple server and immediately start serving forever.
+///
+/// This is equivalent to
+///
+/// ~~~ {.rust}
+/// SimpleServer::new(Config { bind_address: ip_addr }, handler).serve_forever();
+/// ~~~
+///
+/// But it's nicer this way with `do` blocks and closures:
+///
+/// ~~~ {.rust}
+/// do serve_forever(ip_addr) |r, mut w| {
+///     // Now you can handle the request here and write the wresponse.
+/// }
+/// ~~~
+///
+/// The signature of this method is liable to change if `Config` gets more members.
+// Please, pretty please, don't correct the word "wresponse".
+#[inline]
+pub fn serve_forever(ip_addr: IpAddr, handler: ~fn(Request, ResponseWriter)) {
+    SimpleServer::new(Config { bind_address: ip_addr }, handler).serve_forever();
+}
+
+/// 0.0.0.0, port 80: publicly bound to the standard HTTP port.
+/// Not recommended at present as this server is not hardened against the sort of traffic you may
+/// encounter on the Internet and is vulnerable to various DoS attacks. Sit it behind a gateway.
+static PUBLIC: IpAddr = Ipv4(0, 0, 0, 0, 80);
