df86cd0a9ac9716ea9f72928f50eb736a678ecfa
Chris Morgan <me@chrismorgan.info>
2013-07-21 23:36:36 +1000

    Remove a little bit of memory allocation and some dead code.

--- src/librusthttpserver/response.rs
+++ src/librusthttpserver/response.rs
@@ -18,66 +18,6 @@ use super::headers;
 static RESPONSE_HTTP_VERSION: &'static str = "HTTP/1.1";
 // Maybe we could provide a response interface
 
-fn to_owned_bytes(s: ~str) -> ~[u8] {
-    // XXX: Rust compiler bug(?) prevents me from doing `s.as_bytes().to_owned()` in one step
-    let b = s.as_bytes();
-    b.to_owned()
-}
-
-macro_rules! bfmt (
-        ($($expr:expr),*) => (to_owned_bytes(fmt!($($expr),*)))
-        )
-
-/*
-/**
- * A HTTP Response.
- *
- * - `headers`: A hashmap with the headers to send (Content-type and -length are done seperatley).
- * - `status` - A status code for the response. For now only used for the first lne of the response.
- * /
-pub struct Response {
-    headers: ~TreeMap<~str, ~str>,
-    status: status::Status,
-}
-
-impl Response {
-
-    fn init(@mut self) {
-        // Default to text/html
-        //self.headers = ~TreeMap<~str, ~str>::new();
-        self.set_header(~"Content-Type", ~"text/html");
-    }
-
-    fn write_status(&self, socket: &TcpSocket) {
-        socket.write(to_owned_bytes(fmt!("%s %?\r\n", RESPONSE_HTTP_VERSION, self.status)));
-    }
-
-    fn write_headers(&self, socket: &TcpSocket) {
-        for self.headers.iter().advance |(key, value)| {
-            socket.write(to_owned_bytes(fmt!("%s: %s\r\n", *key, *value)));
-        }
-        socket.write(to_owned_bytes(~"\r\n"));
-    }
-
-    fn set_header<V: ToStr>(@mut self, name: ~str, value: V) {
-        // TODO: apply some normalisation to the headers
-        self.headers.insert(name, value.to_str());
-    }
-
-    fn auto_content_length(@mut self) {
-        // Returns true if the key did not already exist in the map, but we don't care
-        self.set_header(~"Content-Length", self.content.len());
-    }
-
-    /// Write the response to a socket using the HTTP version specified by version_number.
-    // TODO: is this different for different versions?
-    pub fn write(&self, version_number: (int, int), socket: &TcpSocket) {
-        self.write_status(socket);
-        self.write_headers(socket);
-        socket.write(to_owned_bytes(copy self.content));
-    }
-}*/
-
 pub struct ResponseWriter {
     // The place to write to (typically a TCP stream, rt::io::net::tcp::TcpStream)
     priv writer: TcpStream,
@@ -117,18 +57,18 @@ impl ResponseWriter {
             fail!("ResponseWriter.write_headers() called, but headers already written");
         }
 
-        // TODO: compare performance of bfmt!("foo %s", ...) and write(bytes!("foo ")) write(...)
-        // (Titchy case, but could be good to get an idea of the perf characteristics; there's an
-        // extremely remote possibility it might affect things later.)
-
         // Write the Status-Line (RFC2616 §6.1)
         // XXX: might be better not to hardcode HTTP/1.1.
-        self.writer.write(bfmt!("HTTP/1.1 %s\r\n", self.status.to_str()));
+        // XXX: Rust's current lack of statement-duration lifetime handling prevents this from being
+        // one statement ("error: borrowed value does not live long enough")
+        let s = fmt!("HTTP/1.1 %s\r\n", self.status.to_str());
+        self.writer.write(s.as_bytes());
 
         // Write the miscellaneous varieties of headers
         // XXX: this is not in the slightest bit sufficient; much more filtration is required.
         for self.headers.iter().advance |(name, value)| {
-            self.writer.write(bfmt!("%s: %s\r\n", *name, *value));
+            let s = fmt!("%s: %s\r\n", *name, *value);
+            self.writer.write(s.as_bytes());
         }
         self.writer.write(bytes!("\r\n"));
         self.headers_written = true;
