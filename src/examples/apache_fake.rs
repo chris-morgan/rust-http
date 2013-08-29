26ddb652ca7d4bdb7b2b55d2e4d643efebabb0b2
Chris Morgan <me@chrismorgan.info>
2013-08-30 09:11:04 +1000

    Don't depend on file EOL for apache_fake correctness.

--- src/examples/apache_fake.rs
+++ src/examples/apache_fake.rs
@@ -52,10 +52,11 @@ impl Server for ApacheFakeServer {
         });
         w.headers.extensions.insert(~"X-Pad", ~"avoid browser bug");
 
-        w.write(bytes!("<html><body><h1>It works!</h1>
-<p>This is the default web page for this server.</p>
-<p>The web server software is running but no content has been added, yet.</p>
-</body></html>\n"));
+        w.write(bytes!("\
+            <html><body><h1>It works!</h1>\n\
+            <p>This is the default web page for this server.</p>\n\
+            <p>The web server software is running but no content has been added, yet.</p>\n\
+            </body></html>\n"));
     }
 }
 
