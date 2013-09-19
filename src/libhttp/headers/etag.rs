5cc993a3945c2658ba0321bf501c35997d2bcb72
Chris Morgan <me@chrismorgan.info>
2013-09-19 19:20:18 +1000

    Add some ETag helper functions and tests.

--- src/libhttp/headers/etag.rs
+++ src/libhttp/headers/etag.rs
@@ -7,6 +7,20 @@ pub struct EntityTag {
     opaque_tag: ~str,
 }
 
+pub fn weak_etag<S: Str>(opaque_tag: S) -> EntityTag {
+    EntityTag {
+        weak: true,
+        opaque_tag: opaque_tag.into_owned(),
+    }
+}
+
+pub fn strong_etag<S: Str>(opaque_tag: S) -> EntityTag {
+    EntityTag {
+        weak: false,
+        opaque_tag: opaque_tag.into_owned(),
+    }
+}
+
 impl ToStr for EntityTag {
     fn to_str(&self) -> ~str {
         if self.weak {
@@ -18,34 +32,26 @@ impl ToStr for EntityTag {
 }
 
 impl super::HeaderConvertible for EntityTag {
-    fn from_stream<T: Reader>(reader: &mut super::HeaderValueByteIterator<T>) -> Option<EntityTag> {
-        let weak;
-        let opaque_tag;
-        match reader.next() {
-            Some(b) if b == 'W' as u8 => match reader.next() {
-                Some(b) if b == '/' as u8 => {
-                    weak = true;
-                    match reader.read_quoted_string(false) {
-                        Some(tag) => opaque_tag = tag,
-                        None => return None,
+    fn from_stream<R: Reader>(reader: &mut super::HeaderValueByteIterator<R>) -> Option<EntityTag> {
+        reader.some_if_consumed(EntityTag {
+            weak: match reader.next() {
+                Some(b) if b == 'W' as u8 => {
+                    if reader.next() != '/' as u8 || reader.next() != '"' {
+                        return None;
                     }
+                    true
                 },
-                _ => return None,
-            },
-            Some(b) if b == '"' as u8 => {
-                weak = false;
-                match reader.read_quoted_string(true) {
-                    Some(tag) => opaque_tag = tag,
-                    None => return None,
+                Some(b) if b == '"' as u8 => {
+                    false
+                },
+                _ => {
+                    return None;
                 }
             },
-            _ => {
-                return None;
+            opaque_tag: match reader.read_quoted_string(true) {
+                Some(tag) => tag,
+                None => return None,
             }
-        };
-        Some(EntityTag {
-            weak: weak,
-            opaque_tag: opaque_tag,
         })
     }
 
@@ -59,4 +65,26 @@ impl super::HeaderConvertible for EntityTag {
     fn http_value(&self) -> ~str {
         self.to_str()
     }
+}
+
+#[test]
+fn test_etag() {
+    use headers::test_utils::*;
+    assert_conversion_correct("\"\"", strong_etag(""));
+    assert_conversion_correct("\"fO0\"", strong_etag("fO0"));
+    assert_conversion_correct("\"fO0 bar\"", strong_etag("fO0 bar"));
+    assert_conversion_correct("\"fO0 \\\"bar\"", strong_etag("fO0 \"bar"));
+    assert_conversion_correct("\"fO0 \\\"bar\\\"\"", strong_etag("fO0 \"bar\""));
+
+    assert_conversion_correct("W/\"\"", weak_etag(""));
+    assert_conversion_correct("W/\"fO0\"", weak_etag("fO0"));
+    assert_conversion_correct("W/\"fO0 bar\"", weak_etag("fO0 bar"));
+    assert_conversion_correct("W/\"fO0 \\\"bar\"", weak_etag("fO0 \"bar"));
+    assert_conversion_correct("W/\"fO0 \\\"bar\\\"\"", weak_etag("fO0 \"bar\""));
+    assert_interpretation_correct("w/\"fO0\"", weak_etag("fO0"));
+
+    assert_invalid::<EntityTag>("");
+    assert_invalid::<EntityTag>("fO0");
+    assert_invalid::<EntityTag>("\"\\\"");
+    assert_invalid::<EntityTag>("\"\"\"\"");
 }
