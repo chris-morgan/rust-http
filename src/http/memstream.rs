653080cb3b8065e17666fbb7c5e5b87999efd2f9
Erik Hedvall <erikwhedvall@gmail.com>
2014-06-13 12:49:04 +0200

    Fixed tests in memstream.rs

--- src/http/memstream.rs
+++ src/http/memstream.rs
@@ -94,18 +94,18 @@ mod test {
     fn test_mem_reader_fake_stream() {
         let mut reader = MemReaderFakeStream::new(vec!(0, 1, 2, 3, 4, 5, 6, 7));
         let mut buf = vec![];
-        assert_eq!(reader.read(buf),      Ok(0));
+        assert_eq!(reader.read(buf.as_mut_slice()),      Ok(0));
         assert_eq!(reader.tell(),         Ok(0));
         let mut buf = vec![0];
-        assert_eq!(reader.read(buf),      Ok(1));
+        assert_eq!(reader.read(buf.as_mut_slice()),      Ok(1));
         assert_eq!(reader.tell(),         Ok(1));
         assert_eq!(buf,                   vec![0]);
         let mut buf = vec![0, 0, 0, 0];
-        assert_eq!(reader.read(buf),      Ok(4));
+        assert_eq!(reader.read(buf.as_mut_slice()),      Ok(4));
         assert_eq!(reader.tell(),         Ok(5));
-        assert_eq!(buf,                   vec[1, 2, 3, 4]);
-        assert_eq!(reader.read(buf),      Ok(3));
+        assert_eq!(buf,                   vec![1, 2, 3, 4]);
+        assert_eq!(reader.read(buf.as_mut_slice()),      Ok(3));
         assert_eq!(buf.slice(0, 3),       &[5, 6, 7]);
-        assert_eq!(reader.read(buf).ok(), None);
+        assert_eq!(reader.read(buf.as_mut_slice()).ok(), None);
     }
 }
